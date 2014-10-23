398b069a70f68751e5ac729d29309d8caa9b8a3a
Jimmy <acsd68500@gmail.com>
2014-10-23 17:39:46 +0800

    have bug in show!!! Couldn't find Clothe with 'id'

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -3,26 +3,30 @@ class ClothesController < ApplicationController
   def index
     @clothes = Clothe.all
   end
+
   def show
     @clothe = Clothe.find(params[:id])
-
   end
+
   def new
     @clothe = Clothe.new
   end
+
   def create
     @clothe = Clothe.new(params_clothes)
     @clothe.save
 
     redirect_to clothes_path
-
   end
+
   def edit
      @clothe = Clothe.find(params[:id])
   end
+
   def update
      @clothe = Clothe.find(params[:id])
   end
+
   def destroy
      @clothe = Clothe.find(params[:id])
   end
